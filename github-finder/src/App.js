e2bab26eaa54e6d59914ef3e47aa4ab0e4cbcfaa
Wei <linkano.teck@gmail.com>
2022-02-15 00:39:47 +1100

    put 2 test project in

---
+++ github-finder/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+
+function App() {
+	return (
+		<Router>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+				<main className='container mx-auto pb-12 px-3'>
+					<Routes>
+						<Route path='/' element={<Home />}></Route>
+						<Route path='/about' element={<About />}></Route>
+						<Route path='/notfound' element={<NotFound />}></Route>
+						<Route path='/*' element={<NotFound />}></Route>
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
+	)
+}
+
+export default App
