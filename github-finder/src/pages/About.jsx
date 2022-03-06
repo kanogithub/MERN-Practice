6b9d4164800dbe4d3202f178d113cdffb9d4045d
Wei <linkano.teck@gmail.com>
2022-03-06 23:15:21 +1100

    update about

--- github-finder/src/pages/About.jsx
+++ github-finder/src/pages/About.jsx
@@ -1,16 +1,29 @@
 function About() {
 	return (
 		<div>
-			<h1 className='text-6xl mb-4'>Github finder</h1>
-			<h3 className='text-xl mb-2'>RESTful API: Github API Practice</h3>
-			<h3 className='text-xl mb-2'>Stack: React.js, TailwindCSS, Daisyui Theme.</h3>
-			<h3 className='text-xl mb-2'>Version: 1.3.0</h3>
-			<p>This is for individual practicing.</p>
-			<p>Mainly focus on fundamental hooks with some popular modern approach.</p>
-			<h3 className='text-xl mt-4 mb-2'>Additional features:</h3>
-			<p>Intersection Observer, Number CountUp, </p>
-			<p>Profile Hover Layout with RWD Button, </p>
-			<p>More Compatibilty with varieties RWD design.,</p>
+			<h1 className='text-6xl mb-4 text-gray-50'>Github finder</h1>
+			<h3 className='text-xl mb-2 text-gray-200'>RESTful API: Github API Practice</h3>
+			<h3 className='text-xl mb-2 text-gray-200'>
+				Stack: React.js, TailwindCSS, Daisyui Theme.
+			</h3>
+			<h3 className='text-xl mb-2 text-gray-200'>Version: 1.3.1</h3>
+			<p className='text-gray-300'>This is for individual practicing.</p>
+			<p className='text-gray-300'>
+				Mainly focus on fundamental hooks with some popular modern approach.
+			</p>
+			<p className='text-gray-300'>
+				Welcome to leave recommandation &gt;&gt;&nbsp;
+				<a
+					className='text-md underline decoration-dashed'
+					href='https://github.com/kanogithub'>
+					Github
+				</a>
+				.
+			</p>
+			<h3 className='text-xl mt-4 mb-2 text-gray-50'>Additional features:</h3>
+			<p className='text-gray-300'>Intersection Observer, Number CountUp, </p>
+			<p className='text-gray-300'>Profile Hover Layout with RWD Button, </p>
+			<p className='text-gray-300'>More Compatible RWD design, Sticky navbar.</p>
 		</div>
 	)
 }
