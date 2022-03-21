f218818f81292a36e5e9138869c0b0088ff425ff
Wei <linkano.teck@gmail.com>
2022-03-22 02:55:08 +1100

    update about page v.2

--- github-finder/src/pages/About.jsx
+++ github-finder/src/pages/About.jsx
@@ -1,13 +1,13 @@
 function About() {
 	return (
 		<div>
-			<h1 className='text-6xl mb-4 text-gray-50'>Github finder</h1>
+			<h1 className='text-6xl mb-8 text-gray-50'>Github finder</h1>
 			<h3 className='text-xl mb-2 text-gray-200'>RESTful API: Github API Practice</h3>
 			<h3 className='text-xl mb-2 text-gray-200'>Custom API: Github Readme Stats</h3>
 			<h3 className='text-xl mb-2 text-gray-200'>
 				Stack: React.js, TailwindCSS, Daisyui Theme.
 			</h3>
-			<h3 className='text-xl mb-2 text-gray-200'>Version: 1.4.2</h3>
+			<h3 className='text-xl mt-8 mb-2 text-gray-200'>Version: 1.4.3</h3>
 			<p className='text-gray-300'>This is for individual practicing.</p>
 			<p className='text-gray-300'>
 				Mainly focus on fundamental hooks with some popular modern approach.
@@ -15,13 +15,13 @@ function About() {
 			<p className='text-gray-300'>
 				Welcome to leave recommandation &gt;&gt;&nbsp;
 				<a
-					className='text-md underline decoration-dashed hover:text-gray-100'
+					className='text-md font-bold underline decoration-sky-300/50 hover:decoration-sky-300/70 hover:text-gray-100 hover:decoration-2'
 					href='https://github.com/kanogithub'>
 					Github
 				</a>
 				.
 			</p>
-			<h3 className='text-xl mt-4 mb-2 text-gray-50'>Additional features:</h3>
+			<h3 className='text-xl mt-8 mb-2 text-gray-50'>Additional features:</h3>
 			<p className='text-gray-300'>Intersection Observer, Number CountUp, </p>
 			<p className='text-gray-300'>Profile Hover Layout with RWD Button, </p>
 			<p className='text-gray-300'>More Compatible RWD design, Sticky navbar.</p>
