4986834ef9b5f3472b8dc801574af5cf7114c004
kanogithub <linkano.teck@gmail.com>
2022-04-05 03:26:08 +1000

    show message when no list

--- github-finder/src/pages/FavorList.jsx
+++ github-finder/src/pages/FavorList.jsx
@@ -6,9 +6,11 @@ function FavorList() {
 		<div className='fade'>
 			<h1 class='text-6xl mb-8 text-gray-50'>Favorite List</h1>
 			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:gridcols-2'>
-				{favorList.map((user) => (
-					<UserItem key={user.id} user={user} />
-				))}
+				{favorList.length === 0 && (
+					<p className='ml-1 text-lg'>You don't build a List yet.</p>
+				)}
+				{favorList.length > 0 &&
+					favorList.map((user) => <UserItem key={user.id} user={user} />)}
 			</div>
 		</div>
 	)
