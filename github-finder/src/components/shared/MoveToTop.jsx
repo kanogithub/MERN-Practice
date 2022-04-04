64c4135309e8923390f2b5f6e2fcc45560feb49b
kanogithub <linkano.teck@gmail.com>
2022-04-05 04:43:08 +1000

    impove RWD design for mobile

--- github-finder/src/components/shared/MoveToTop.jsx
+++ github-finder/src/components/shared/MoveToTop.jsx
@@ -3,6 +3,8 @@ import { FaLongArrowAltUp } from 'react-icons/fa'
 
 function MoveToTop() {
 	const moveButton = useRef()
+	const elem = moveButton.current
+
 	const moveToTop = () => {
 		window.scrollTo({
 			top: 0,
@@ -10,24 +12,37 @@ function MoveToTop() {
 		})
 	}
 
+	const debounce = (func, delay) => {
+		let inDebounce
+		return function () {
+			const context = this
+			const args = arguments
+			clearTimeout(inDebounce)
+			inDebounce = setTimeout(() => func.apply(context, args), delay)
+		}
+	}
+
+	const closeMoveTopTimeout = debounce(() => elem.classList.remove('show'), 2500)
+
 	const handleScroll = () => {
 		const pageY = window.innerHeight
 		const scrollY = window.scrollY
-		const elem = moveButton.current
 
 		scrollY > (pageY * 2) / 3 ? elem.classList.add('show') : elem.classList.remove('show')
+		closeMoveTopTimeout()
 	}
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll)
 
 		return () => window.removeEventListener('scroll', handleScroll)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	return (
 		<button
 			ref={moveButton}
-			className='btn btn-ghost text-lg opacity-25 hover:text-gray-200 hover:opacity-100 moveButton'
+			className='btn btn-ghost text-lg hover:text-gray-200 hover:opacity-100 moveButton'
 			onClick={moveToTop}>
 			<FaLongArrowAltUp style={{ margin: 'auto' }} />
 			<span>Move to Top</span>
