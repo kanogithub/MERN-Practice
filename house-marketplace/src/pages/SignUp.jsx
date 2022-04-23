d88e103b554fa7180e596d6ba5a85fe6663c8575
kanogithub <linkano.teck@gmail.com>
2022-04-23 18:16:49 +1000

    market app basic layout & sign in/up pages

---
+++ house-marketplace/src/pages/SignUp.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
+function SignUp() {
+	const [showPassword, setShowPassword] = useState(false)
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+	})
+
+	const { name, email, password } = formData
+	const onChange = (e) =>
+		setFormData((preValue) => ({ ...preValue, [e.target.id]: e.target.value }))
+
+	return (
+		<>
+			<div className='pageContainer'>
+				<header>
+					<p className='pageHeader'>Welcome Back!</p>
+				</header>
+
+				<form>
+					<input
+						type='text'
+						className='nameInput'
+						placeholder='Name'
+						id='name'
+						value={name}
+						onChange={onChange}
+					/>
+					<input
+						type='email'
+						className='emailInput'
+						placeholder='Email'
+						id='email'
+						value={email}
+						onChange={onChange}
+					/>
+
+					<div className='passwordInputDiv'>
+						<input
+							type={showPassword ? 'text' : 'password'}
+							className='passwordInput'
+							placeholder='Password'
+							id='password'
+							value={password}
+							onChange={onChange}
+						/>
+
+						<img
+							src={visibilityIcon}
+							alt='show password'
+							className='showPassword'
+							onClick={() => setShowPassword((preV) => !preV)}
+						/>
+					</div>
+
+					<Link to='/forgot-password' className='forgotPasswordLink'>
+						Forgot Password
+					</Link>
+
+					<div className='signUpBar'>
+						<p className='signUpText'>Sign Up</p>
+						<button className='signUpButton'>
+							<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+						</button>
+					</div>
+				</form>
+
+				{/* Google OAuth */}
+
+				<Link to='/sign-in' className='registerLink'>
+					Sign In Instead
+				</Link>
+			</div>
+		</>
+	)
+}
+
+export default SignUp
