02836c066b6ccaed31b8ce58e77c64cfd276cfbc
kanogithub <linkano.teck@gmail.com>
2022-04-23 22:00:02 +1000

    firestore sign up test

--- house-marketplace/src/pages/SignUp.jsx
+++ house-marketplace/src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -12,8 +14,28 @@ function SignUp() {
 	})
 
 	const { name, email, password } = formData
+
+	const navigate = useNavigate()
+
 	const onChange = (e) =>
 		setFormData((preValue) => ({ ...preValue, [e.target.id]: e.target.value }))
+
+	const onSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			const auth = getAuth()
+			const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+			const user = userCredential.user
+			updateProfile(auth.currentUser, {
+				displayName: name,
+			})
+			// console.log(user)
+			navigate('/')
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	return (
 		<>
@@ -22,7 +44,7 @@ function SignUp() {
 					<p className='pageHeader'>Welcome Back!</p>
 				</header>
 
-				<form>
+				<form onSubmit={onSubmit}>
 					<input
 						type='text'
 						className='nameInput'
