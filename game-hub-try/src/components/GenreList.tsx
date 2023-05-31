491f2aad9ec9ce3b66fac47d199796d77ecc9143
Teng-Wei Huang <linkano.teck@gmail.com>
2023-05-31 22:19:57 +1000

    refine better genre list

--- game-hub-try/src/components/GenreList.tsx
+++ game-hub-try/src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, Image, HStack, Text, Button, Link } from '@chakra-ui/react'
+import { List, ListItem, Image, HStack, Text, Button, Link, Heading } from '@chakra-ui/react'
 import GenreSkeleton from './GenreSkeleton'
 import getCorppedImageUrl from '../utilities/image-url'
 import useGenres, { Genre } from '../hooks/useGenres'
@@ -15,35 +15,45 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 	if (error) return null
 
 	return (
-		<List>
-			{isLoading
-				? skeletons.map((skeleton) => (
-						<ListItem key={skeleton} paddingY={2}>
-							<GenreSkeleton />
-						</ListItem>
-				  ))
-				: data.map((genre) => (
-						<ListItem key={genre.id} paddingY={2}>
-							<HStack>
-								<Image
-									boxSize='32px'
-									borderRadius={8}
-									src={getCorppedImageUrl(genre.image_background)}
-								/>
-								<Text
-									fontSize='lg'
-									fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-									color={genre.id === selectedGenre?.id ? '#4863A0' : ''}
-									_hover={{ cursor: 'pointer', textDecoration: 'underline' }}
-									onClick={() =>
-										onSelectGenre(genre.id === selectedGenre?.id ? null : genre)
-									}>
-									{genre.name}
-								</Text>
-							</HStack>
-						</ListItem>
-				  ))}
-		</List>
+		<>
+			<Heading fontSize='2xl' marginBottom={3}>
+				Genres
+			</Heading>
+			<List>
+				{isLoading
+					? skeletons.map((skeleton) => (
+							<ListItem key={skeleton} paddingY={2}>
+								<GenreSkeleton />
+							</ListItem>
+					  ))
+					: data.map((genre) => (
+							<ListItem key={genre.id} paddingY={2}>
+								<HStack>
+									<Image
+										boxSize='32px'
+										borderRadius={8}
+										objectFit='cover'
+										src={getCorppedImageUrl(genre.image_background)}
+									/>
+									<Text
+										fontSize='lg'
+										fontWeight={
+											genre.id === selectedGenre?.id ? 'bold' : 'normal'
+										}
+										color={genre.id === selectedGenre?.id ? '#4863A0' : ''}
+										_hover={{ cursor: 'pointer', textDecoration: 'underline' }}
+										onClick={() =>
+											onSelectGenre(
+												genre.id === selectedGenre?.id ? null : genre
+											)
+										}>
+										{genre.name}
+									</Text>
+								</HStack>
+							</ListItem>
+					  ))}
+			</List>
+		</>
 	)
 }
 
