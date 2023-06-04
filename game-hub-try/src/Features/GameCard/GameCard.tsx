a0c1a1fed7f6573ffc1c4c10d646b4102ec8da38
Teng-Wei Huang <linkano.teck@gmail.com>
2023-06-04 22:33:09 +1000

    clean up game card

--- game-hub-try/src/Features/GameCard/GameCard.tsx
+++ game-hub-try/src/Features/GameCard/GameCard.tsx
@@ -17,11 +17,11 @@ const GameCard = ({ game }: Props) => {
 			<Image src={getCorppedImageUrl(game.background_image)} />
 			<GenresMark show={hoverOnCard} genres={game.genres} />
 			<CardBody>
-				<Heading fontSize='2xl'>{game.name}</Heading>
-				<HStack justifyContent='space-between'>
+				<HStack justifyContent='space-between' marginBottom={2}>
 					<PlatformList platforms={game.parent_platforms.map((p) => p.platform)} />
 					<CriticScore score={game.metacritic} />
 				</HStack>
+				<Heading fontSize='2xl'>{game.name}</Heading>
 			</CardBody>
 		</Card>
 	)
